864488462e4c7b95249b29fa99cb9631d4cfa311
Oleg Karpenko <olkieah1@gmail.com>
2026-07-21 17:23:52 +0200

    Updated Verlet Step to use new force earlier

--- src/md.py
+++ src/md.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 def verlet_step(polymer, dt, force_func):
-    polymer.v += dt*polymer.f/polymer.m
+    polymer.v += dt*polymer.f/polymer.m * 0.5
 
     polymer.r += dt * polymer.v
     polymer.r %= polymer.box
 
     force_func(polymer)
+    polymer.v += dt*polymer.f/polymer.m * 0.5
 
